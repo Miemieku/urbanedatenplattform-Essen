977e3289d517c3991c5a13985767ac215c004c24
JW_He <61524560+Miemieku@users.noreply.github.com>
2025-07-22 14:41:03 +0200

    11

--- netlify/functions/supabaseProxy.js
+++ netlify/functions/supabaseProxy.js
@@ -1,10 +1,13 @@
 // netlify/functions/supabaseProxy.js
 
 export async function handler(event, context) {
-  const response = await fetch("https://qmjzzsmzvawnxyuxajbg.supabase.co/rest/v1/stadtteilgrenzen_geojson?select=*", {
+    const API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFqbXp6c216dmF3bnh5dXhhamJnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI0OTY0NjcsImV4cCI6MjA2ODA3MjQ2N30.VHPh9C2NEQIpOT5xy8mP7wfJyhDXUEZaf2IiDI1c3L4"
+    const SUPABASE_URL = "https://qjmzzsmzvawnxyuxajbg.supabase.co"
+    const response = await fetch(`${SUPABASE_URL}/rest/v1/stadtteilgrenzen_geojson?select=*`, {
     headers: {
-      apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFqbXp6c216dmF3bnh5dXhhamJnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI0OTY0NjcsImV4cCI6MjA2ODA3MjQ2N30.VHPh9C2NEQIpOT5xy8mP7wfJyhDXUEZaf2IiDI1c3L4",
-      Authorization: `Bearer ${API_KEY}`
+      apikey: API_KEY,
+      Authorization: `Bearer ${API_KEY}`,
+      Accept: "application/json"
     }
   });
 
