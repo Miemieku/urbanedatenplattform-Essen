7ca74d55f422429834418dc5cda86baf3dba9eea
JW_He <61524560+Miemieku@users.noreply.github.com>
2025-07-30 12:15:46 +0200

    123

--- public/airQuality.js
+++ public/airQuality.js
@@ -262,8 +262,8 @@ function addStationsToMap() {
                     actualTimestamp,
                     pollutantData,
                     stationId,
-                    dateto,
-                    timeto
+                    result.enddate,
+                    result.endtime
                 );
             });
 
